d66c9e55ca5682937c7b770ecdb7c329ed446f3d
shutyforce21 <bbshuty.devlife@gmail.com>
2022-01-02 21:11:01 +0900

    ユーザー新規登録 FeatureTest 作成 green

--- app/tests/Feature/AuthController/RegisterTest.php
+++ app/tests/Feature/AuthController/RegisterTest.php
@@ -1,12 +1,70 @@
 <?php
 
-
 namespace Tests\Feature\AuthController;
 
-
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
 {
+    use DatabaseTransactions;
 
+    /**
+     * @test
+     * @dataProvider initData
+     */
+    public function 「正常系」ユーザーが新規登録する($data)
+    {
+        $loginResponse = $this->postJson('/api/user/register', $data);
+        $loginResponse->assertSuccessful();
+
+        // ユーザー情報が保存されているか
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email']
+        ]);
+
+        $userModel = User::orderBy('created_at', 'desc')->first();
+
+        $bool = password_verify($data['password'], $userModel->password);
+        $this->assertTrue($bool);
+    }
+
+    /**
+     * @test
+     * @dataProvider initData
+     */
+    public function 「異常系」ユーザーが新規登録する($data)
+    {
+        // パスワード制約: 大文字、小文字、数字
+        $data['password'] = 'asdf1234';
+        $loginResponse = $this->postJson('/api/user/register', $data);
+
+        //バリデーションエラー
+        $this->assertEquals(
+            Response::HTTP_BAD_REQUEST,
+            $loginResponse->getStatusCode()
+        );
+    }
+
+
+    /**
+     * ユーザー新規作成データ
+     * @return \Generator
+     */
+    public function initData()
+    {
+        yield [
+            'data' => [
+                'name' => 'テストダンサー',
+                'email' => 'testEmail@example.com',
+                'password' => 'Asdf1234',
+                'password_confirmation' => 'Asdf1234',
+            ]
+        ];
+    }
 }
