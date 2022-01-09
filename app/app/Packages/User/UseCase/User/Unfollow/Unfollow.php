76b3c9e40b3ec9f808cab3c4ed5e49d460ff501b
shutyforce21 <bbshuty.devlife@gmail.com>
2022-01-09 16:57:30 +0900

    アンフォロー FeatureTest 更新 Red

---
+++ app/app/Packages/User/UseCase/User/Unfollow/Unfollow.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Packages\User\UseCase\User\Unfollow;
+
+use App\Packages\User\Infrastructure\User\RepositoryInterface;
+
+class Unfollow
+{
+    private $repository;
+
+    public function __construct(RepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param $authId
+     * @param $followedId
+     * @throws \Exception
+     */
+    public function handle($authId, $followedId)
+    {
+        $user = $this->repository->findById($authId);
+        $user->unfollow($followedId);
+        $this->repository->unfollowAndSave($user);
+    }
+}
