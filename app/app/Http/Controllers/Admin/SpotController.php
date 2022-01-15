73fdb9ac4c5fff3e01b61b740389b49fb5db2528
shutyforce21 <bbshuty.devlife@gmail.com>
2022-01-15 16:18:59 +0900

    web routing 整理

--- app/app/Http/Controllers/Admin/SpotController.php
+++ app/app/Http/Controllers/Admin/SpotController.php
@@ -6,7 +6,29 @@ namespace App\Http\Controllers\Admin;
 
 class SpotController
 {
+    /**
+     * 一覧
+     * @return string
+     */
     public function index()
+    {
+        return 'asdf';
+    }
+
+    /**
+     * 有効化
+     * @return string
+     */
+    public function activate()
+    {
+        return 'asdf';
+    }
+
+    /**
+     * 無効化
+     * @return string
+     */
+    public function inactivate()
     {
         return 'asdf';
     }
--- app/routes/web.php
+++ app/routes/web.php
@@ -2,20 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| 管理画面
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//管理画面
 Route::get('/admin/spots', [\App\Http\Controllers\Admin\SpotController::class, 'index']);
+Route::get('/admin/spots/activate', [\App\Http\Controllers\Admin\SpotController::class, 'activate']);
+Route::get('/admin/spots/inactivate', [\App\Http\Controllers\Admin\SpotController::class, 'inactivate']);
+
