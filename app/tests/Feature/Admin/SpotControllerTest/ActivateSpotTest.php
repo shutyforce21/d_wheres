4aefa8451608fc3bcd6e645ccca8c887023698e0
shutyforce21 <bbshuty.devlife@gmail.com>
2022-02-04 23:58:40 +0900

    FeatuTest

--- app/tests/Feature/Admin/SpotControllerTest/ActivateSpotTest.php
+++ app/tests/Feature/Admin/SpotControllerTest/ActivateSpotTest.php
@@ -4,6 +4,7 @@
 namespace Tests\Feature\Admin\SpotControllerTest;
 
 
+use App\Models\Spot;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -30,6 +31,23 @@ class ActivateSpotTest extends TestCase
      */
     public function 「正常系」総合管理者がスポットを有効化する()
     {
+        $inactiveSpotId = 41;
+        $inactiveSpot = Spot::find($inactiveSpotId);
 
+        //有効フラグがfalse
+        $this->assertFalse($inactiveSpot->active);
+
+        $loginResponse = $this->putJson(
+            '/admin/spots/41/activate',
+            ['email' => 'asdf1@asdf.com', 'password' => 'password']
+        );
+        $loginResponse->assertSuccessful();
+
+
+        $inactiveSpotId = 41;
+        $inactiveSpot = Spot::find($inactiveSpotId);
+
+        //有効フラグがfalse
+        $this->assertTrue($inactiveSpot->active);
     }
 }
