40e52b61545e6ddc9d5ed8da33e04ee34bbd6acd
shutyforce21 <bbshuty.devlife@gmail.com>
2022-02-05 00:25:24 +0900

    Role Enum 追加

---
+++ app/app/Enums/Role.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Enums;
+
+
+use BenSampo\Enum\Enum;
+
+class Role extends Enum
+{
+    const Administrator = 'administrator';
+    const SuperAdministrator = 'super_administrator';
+}
