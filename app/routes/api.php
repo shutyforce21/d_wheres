0669d17a623eb00a59ffdc368a970eaee17ebaf9
shutyforce21 <bbshuty.devlife@gmail.com>
2022-02-20 07:29:16 +0900

    修正

--- app/routes/api.php
+++ app/routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\SpotController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
